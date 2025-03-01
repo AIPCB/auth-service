a065b6fd284caa9ea64d7a5b9eff58f66023491b
exception-raised <dimmelan@riseup.net>
2025-03-01 10:40:23 +0200

    Add login endpoint to routes

--- src/server/routes.go
+++ src/server/routes.go
@@ -2,5 +2,6 @@ package server
 
 func (s *Server) RegisterRoutes() {
 	s.router.HandleFunc("/register", s.RegisterHandler()).Methods("POST")
+	s.router.HandleFunc("/login", s.LoginHandler()).Methods("POST")
 
 }
