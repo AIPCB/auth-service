8f27719ddf4624662937700870a8478b5aa8929f
exception-raised <135153991+exception-raised@users.noreply.github.com>
2025-03-01 10:45:19 +0200

    AIPC-3: Login endpoint (#3)
    * Add login endpoint to routes
    
    * Add LoginRequest model for handling login requests
    
    * Add LoginHandler for processing login requests
    
    * add todo comment
    
    * update typo
    
    ---------
    
    Co-authored-by: exception-raised <dimmelan@riseup.net>

--- src/server/handlers.go
+++ src/server/handlers.go
@@ -22,3 +22,19 @@ func (s *Server) RegisterHandler() http.HandlerFunc {
 		json.NewEncoder(w).Encode(req)
 	}
 }
+
+func (s *Server) LoginHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req models.LoginRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		// todo: validate request and implement actual logic for login
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(req)
+	}
+}
