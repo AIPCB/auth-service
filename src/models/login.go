4c59882992452216ded887ce8e229a4f985fa135
exception-raised <135153991+exception-raised@users.noreply.github.com>
2025-03-01 21:04:16 +0200

    AIPC-4: Add validation to endpoints (#4)
    * Add validation module dependency in go.mod
    
    * Add validation methods for login and register requests
    
    * Add request validation for login and registration handlers
    
    ---------
    
    Co-authored-by: exception-raised <dimmelan@riseup.net>

--- src/models/login.go
+++ src/models/login.go
@@ -1,6 +1,20 @@
 package models
 
+import validator "github.com/exception-raised/validation-module/src"
+
 type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (rr *LoginRequest) Validate() string {
+	if !validator.IsValidEmail(rr.Email) {
+		return "Invalid email"
+	}
+
+	if !validator.IsValidPassword(rr.Password, 8, 32, "") {
+		return "Invalid password"
+	}
+
+	return ""
 }
