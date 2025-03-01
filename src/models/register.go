d78f42b302d38c9bbef39f9e2edd962417e41d59
exception-raised <dimmelan@riseup.net>
2025-03-01 20:59:06 +0200

    Add validation methods for login and register requests

--- src/models/register.go
+++ src/models/register.go
@@ -1,6 +1,22 @@
 package models
 
+import (
+	validator "github.com/exception-raised/validation-module/src"
+)
+
 type RegisterRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (rr *RegisterRequest) Validate() string {
+	if !validator.IsValidEmail(rr.Email) {
+		return "Invalid email"
+	}
+
+	if !validator.IsValidPassword(rr.Password, 8, 32, "") {
+		return "Invalid password"
+	}
+
+	return ""
 }
