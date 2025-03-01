8c8244ddeb0c85fd976e27466947bf1c029387da
exception-raised <dimmelan@riseup.net>
2025-03-01 10:28:33 +0200

    add RegisterHandler for user registration

--- src/server/handlers.go
+++ src/server/handlers.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/AIPCB/auth-service/src/models"
+)
+
+func (s *Server) RegisterHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req models.RegisterRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+
+		// todo: validate request and implement actual logic for registration
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(req)
+	}
+}
