3eab2a8b7f6d53016df79e8e6dea9481127a558e
exception-raised <135153991+exception-raised@users.noreply.github.com>
2025-03-03 20:42:32 +0200

    AIPC-5: Database setup (#5)
    * Update go.mod and go.sum to include additional indirect dependencies
    
    * Add User model with email and password fields
    
    * Add Docker Compose configuration for PostgreSQL service
    
    * Refactor Postgres repository to use GORM instead of database/sql
    
    * Initialize GORM database connection and auto-migrate User model
    
    * Add godotenv dependency for environment variable management
    
    * Update Docker Compose configuration to use environment variables for PostgreSQL settings
    
    * Add .gitignore to exclude data directory and environment file
    
    * Load environment variables from .env file for database connection configuration
    
    * Add error handling for database migration in server initialization
    
    ---------
    
    Co-authored-by: exception-raised <dimmelan@riseup.net>

--- src/cmd/server/server.go
+++ src/cmd/server/server.go
@@ -2,20 +2,45 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/AIPCB/auth-service/src/models"
 	"github.com/AIPCB/auth-service/src/repo"
 	"github.com/AIPCB/auth-service/src/server"
 	"github.com/AIPCB/auth-service/src/service"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+// TODO: Break up into smaller pieces
 func Execute() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
-	authService := service.NewAuthService(service.WithRepo(repo.NewPostgresRepo(nil)))
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// TODO: abstract, use secure connection
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_SSLMODE"))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("Failed to open database: %+v", err)
+	}
+
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		log.Fatalf("Failed to migrate database: %+v", err)
+	}
+
+	authService := service.NewAuthService(service.WithRepo(repo.NewPostgresRepo(db)))
 
 	s := server.NewServer(
 		server.WithAuthService(authService),
