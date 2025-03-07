950ce35700c4c9bc6ddc1ed62cbf31266cd5c307
exception-raised <dimmelan@riseup.net>
2025-03-07 20:35:34 +0200

    Remove unused repository option from AuthService

--- src/service/options.go
+++ src/service/options.go
@@ -1,11 +1,3 @@
 package service
 
-import "github.com/AIPCB/auth-service/src/repo"
-
 type Option func(*AuthService)
-
-func WithRepo(repo repo.AuthRepo) Option {
-	return func(service *AuthService) {
-		service.repo = repo
-	}
-}
