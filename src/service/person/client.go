f062d59edfa602a7ee99e078ba0d966d9efb7188
exception-raised <dimmelman@riseup.net>
2025-05-21 19:39:19 +0200

    feat: implement PersonService with creation functionality and options

---
+++ src/service/person/client.go
@@ -0,0 +1,43 @@
+package person
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+
+	"github.com/AIPCB/auth-service/src/models"
+	httpclient "github.com/AIPCB/shared/src/http"
+)
+
+type PersonService struct {
+	url    *url.URL
+	client *httpclient.Client
+}
+
+func NewPersonService(opts ...Option) (*PersonService, error) {
+	s := &PersonService{}
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	if s.url == nil {
+		return nil, errors.New("service: missing storage")
+	}
+
+	s.client = httpclient.New(s.url.String())
+
+	return s, nil
+}
+
+func (s *PersonService) CreatePerson(ctx context.Context, req models.RegisterRequest) error {
+	resp := &models.RegisterResponse{}
+	err := s.client.DoRequest(ctx, "POST", "create-person", req, resp)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(resp)
+	return nil
+
+}
